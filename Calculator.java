80f9e3879ce08723ef390cfea8c83913e666d688
MouaLor45 <112673717+MouaLor45@users.noreply.github.com>
2024-09-24 15:30:18 -0500

    gitpush

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args){
+
+        Scanner scrn = new Scanner(System.in);
+
+        int x = scrn.nextInt();
+        int y = scrn.nextInt();
+
+
+        System.out.print(x+y);
+
+    } 
+}
+
+
